f56390104c9a8909e8440c3dd34de2152338ad5b
s7eel <s7eel@me>
2017-04-30 13:07:25 +0300

    data blog issues

--- Library/Blog.php
+++ Library/Blog.php
@@ -10,11 +10,58 @@ class Blog extends DataBase
 {
     public function getFullBlogItems()
     {
-        $query = "SELECT * FROM blog ORDER BY created_at ASC LIMIT 5";
+        $query = "SELECT * FROM blog ORDER BY created_at DESC LIMIT 5";
         if($result = parent::arrayRes($query)){
         return $result;
         }else{
             return FALSE;
         }
+    }
+
+    public function currentDate()
+    {
+        //                -----------способ 2-------------------------
+        //                этот код определяет текущую дату
+        ////      его можно заносить в бд вместе со статьёй
+        $months = array(
+            "1"=>"Январь",
+            "2"=>"Февраль",
+            "3"=>"Март",
+            "4"=>"Апрель",
+            "5"=>"Май",
+            "6"=>"Июнь",
+            "7"=>"Июль",
+            "8"=>"Август",
+            "9"=>"Сентябрь",
+            "10"=>"Октябрь",
+            "11"=>"Ноябрь",
+            "12"=>"Декабрь",
+        );
+        $day = date("d");
+        $mon = $months[date("n")];
+        $year = date("Y");
+        return $data_blog=" "."$day"." "."$mon"." "."$year"." ";
+    }
+
+    public function getDataFromDB($data)
+    {
+        $data_blog1 = explode("-", $data);
+        switch($data_blog1[1])
+        {
+            case '01':          $data_blog1[1]="января";     break;
+            case '02':          $data_blog1[1]="февраля";    break;
+            case '03':          $data_blog1[1]="марта";       break;
+            case '04':          $data_blog1[1]="апреля";     break;
+            case '05':          $data_blog1[1]="мая";        break;
+            case '06':          $data_blog1[1]="июня";       break;
+            case '07':          $data_blog1[1]="июля";       break;
+            case '08':          $data_blog1[1]="августа";     break;
+            case '09':          $data_blog1[1]="сентября";   break;
+            case '10':          $data_blog1[1]="октября";    break;
+            case '11':          $data_blog1[1]="ноября";     break;
+            case '12':          $data_blog1[1]="декабря";    break;
+            default:            $data_blog1[1]="";
+        }
+        return $data_blog1[2]." ".$data_blog1[1]." ".$data_blog1[0];
     }
 }
--- pages/blog.php
+++ pages/blog.php
@@ -1,67 +1,13 @@
 <div class="directions_header">
     <p>Блог</p>
 </div>
-<?php $res = $blog->getFullBlogItems();
-//var_dump($res);?>
 <div class="container">
-<!--    <div class="article"-->
         <?php foreach ($blog->getFullBlogItems() as $key => $item):?>
         <img class="article_title_short" src="../img/blog/<?= $item['link_foto'];?>" width="300px" height="200px">
         <p class="markh3b"><?= $item['title']; ?></p>
-<!--        <p class="article_data">--><?//= $item['created_at']; ?><!--</p>-->
-        <p class="article_data"><?php
-//                -----------способ 2-------------------------
-//                этот код определяет текущую дату
-                ////      его можно заносить в бд вместе со статьёй
-                $months = array(
-                     "1"=>"Январь",
-                     "2"=>"Февраль",
-                     "3"=>"Март",
-                     "4"=>"Апрель",
-                     "5"=>"Май",
-                     "6"=>"Июнь",
-                     "7"=>"Июль",
-                     "8"=>"Август",
-                     "9"=>"Сентябрь",
-                    "10"=>"Октябрь",
-                    "11"=>"Ноябрь",
-                    "12"=>"Декабрь"
-                );
-                $day = date("d");
-                $mon = $months[date("n")];
-                $year = date("Y");
-                $data_blog=" "."$day"." "."$mon"." "."$year"." ";
-//                echo ( $data_blog);
-//-------------------------способ 1-------------------------------
-
-//                echo $item['created_at'];
-                $data1 = $item['created_at'];
-                $data_blog1 = explode("-", $data1);
-                $data_m= $data_blog1[1];
-//                echo ($data_m);
-                    switch($data_m)
-                    {
-                        case '01':          $blog_mon="Январь";     break;
-                        case '02':          $blog_mon="Февраль";    break;
-                        case '03':          $blog_mon="Март";       break;
-                        case '04':          $blog_mon="Апрель";     break;
-                        case '05':          $blog_mon="Май";        break;
-                        case '06':          $blog_mon="Июнь";       break;
-                        case '07':          $blog_mon="Июль";       break;
-                        case '08':          $blog_mon="Август";     break;
-                        case '09':          $blog_mon="Сентябрь";   break;
-                        case '10':          $blog_mon="Октябрь";    break;
-                        case '11':          $blog_mon="Ноябрь";     break;
-                        case '12':          $blog_mon="Декабрь";    break;
-                        default: return     $blog_mon=" ";
-                    }
-                ?>
-            <?=$data_blog1[2]." ".$blog_mon." ".$data_blog1[0]?></p>
+        <p class="article_data"><?= $blog->getDataFromDB($item['created_at'])?></p>
         <div class="article _short">
             <p><?= $item['full_description']; ?></p>
         </div>
         <?php endforeach; ?>
-
-
-
 </div>
