a6f748a382ff6e2784ff3a359307102eb7751ad4
Marinailch <marina_ilch@mail.ru>
2017-05-06 00:31:15 +0300

    блог слайдер

---
+++ pages/slider_blog.php
@@ -0,0 +1,46 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel" style="height: 500px; overflow: hidden;">
+    <!-- Indicators -->
+    <ol class="carousel-indicators" >
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+ <!--        <li data-target="#myCarousel" data-slide-to="4"></li> -->
+      <!--   <li data-target="#myCarousel" data-slide-to="5"></li> -->
+
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+         <div class="item active">
+             <a href=""><img src="img/blog/blog_img1.jpg" alt="" ></a>
+        </div>
+
+  		<div class="item">
+             <a href=""><img src="img/blog/blog_img2.jpg" alt="" ></a>
+        </div>
+
+         <div class="item">
+             <a href=""><img src="img/blog/blog_img3.jpg" alt=""  ></a>
+        </div>
+
+         <div class="item">
+             <a href=""><img src="img/blog/blog1.jpg" alt="" ></a>
+        </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev" style="
+   ">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next" style="
+   ">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
