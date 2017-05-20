18e4e3287d675c4e83f9af99761a10d731034276
Marinailch <marina_ilch@mail.ru>
2017-05-20 12:57:23 +0300

    Admin add files

---
+++ Admin/header_adm.php
@@ -0,0 +1,49 @@
+   <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>ADMIN panel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Еврейский Медицинский Центр">
+        <link rel="stylesheet" href="../css/bootstrap.min.css" media="all"/>
+        <link rel="stylesheet" href="style_adm.css">
+        <script src="https://cdn.ckeditor.com/4.6.2/full-all/ckeditor.js"></script>
+         <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+         <script src="../js/jquery-3.1.1-jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+    <header>
+<div class="navbar navbar-default navbar-fixed-top" role="navigation" id="slide-nav" >
+            <div class="container">
+            <div class="navbar-header" >
+                <a href=""><img class="logo_head" src="../img/logo.gif"></a>
+
+                <div id="slidemenu">
+                    <ul class="nav navbar-nav">
+                        <li><a href="">Главная</a></li>
+                       <li><a href="">Услуги</a></li>
+                        <li><a href="" class="dropdown-toggle"data-toggle="dropdown">Диагностика <b class="caret"></b></a>
+                            <ul class="dropdown-menu dropdown">
+                                <li><a href="" class="">УЗИ</a></li>
+                                <li><a href="" class="">Лабораторная</a></li>
+                                <li><a href="" class="">Функциональная</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Врачи</a></li>
+                        <li><a href="">Блог</a></li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">О нас <b class="caret"></b></a>
+                            <ul class="dropdown-menu ">
+                                <li><a href="" class="">О клинике</a></li>
+                                <li><a href="" class="">Руководство</a></li>
+                                <li><a href="" class="">Раввин о нас</a></li>
+                                <li><a href="" class="">Контакты</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    </header>
---
+++ Admin/main_adm.php
@@ -0,0 +1,57 @@
+<nav id="main">
+        <form class="form-horizontal" method="POST" action="index.php">
+
+
+            <!--        <textarea name="test"></textarea>-->
+            <!---->
+            <!--        <div class="form-group">-->
+            <!--            <label for="inputSuccess3">Биоматериал</label>-->
+            <!--            <div class="col-sm-4">-->
+            <!--                <input type="text" name="bio" class="form-control">-->
+            <!---->
+            <!---->
+            <!--            </div>-->
+            <!--        </div>-->
+            <!--        <div class="form-group">-->
+            <!--            <label for="inputSuccess3">Результат</label>-->
+            <!--            <div class="col-sm-4">-->
+            <!--                <input type="text" name="result" class="form-control">-->
+            <!---->
+            <!---->
+            <!--            </div>-->
+            <!--        </div>-->
+            <!---->
+            <!--        <div class="form-group">-->
+            <!--            <label for="inputSuccess3">Срок</label>-->
+            <!--            <div class="col-sm-4">-->
+            <!--                <input type="text" name="term" class="form-control">-->
+            <!---->
+            <!---->
+            <!--            </div>-->
+            <!--        </div>-->
+            <!---->
+            <!--        <div class="form-group">-->
+            <!--            <label for="inputSuccess3">Цена</label>-->
+            <!--            <div class="col-sm-4">-->
+            <!--                <input type="text" name="price" class="form-control">-->
+            <!---->
+            <!---->
+            <!--            </div>-->
+            <!--        </div>-->
+
+
+            <!-- Trying new action-->
+
+            <div class="form-group">
+                <label for="inputSuccess3">Название модуля лаб исследований</label>
+                <div class="col-sm-4">
+                    <input type="text" name="name" class="form-control">
+                </div>
+            </div>
+
+            <textarea name="test"></textarea>
+            <!-- Trying new action-->
+
+            <input type="submit" class="form-horizontal" name="submit">
+        </form>
+	</nav>
