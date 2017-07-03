a4f35c73caa746e5ebb4e80db819c019aefad905
Marinailch <marina_ilch@mail.ru>
2017-07-04 00:34:51 +0300

    price

--- Admin/layouts/consultation.php
+++ Admin/layouts/consultation.php
@@ -8,22 +8,14 @@
             <h3>Создание нового прайса без выезда на дом</h3>
             <form method="POST">
                 <table class="table  ">
-                    <tr class="1">
-                        <th>Описание</th>
-                        <th>Цена/1 посещение</th>
-                        <th>Цена/последующие</th>
-                        <th>Специальность</th>
-                        <th></th>
-                    </tr>
+
                     <tr>
-                        <td class="" style="width: 200px; border-top: none; height: 35px;"><input style="height: 35px;" type="text" name="specialty"></td>
-                        <td class="" style="width: 200px; border-top: none; height: 35px;"><input style="height: 35px;" type="text" name="price_first_time"></td>
-                        <td class="" style="width: 200px; border-top: none; height: 35px;"><input style="height: 35px;" type="text" name="price_after"></td>
+                        <td class="" style="width: 40%; border-top: none; height: 35px;"><input style="height: 35px; width: 100%" placeholder=" Описание" type="text" name="specialty"></td>
+                        <td class="" style="width: 15%; border-top: none; height: 35px;"><input style="height: 35px; width: 100%" placeholder=" Цена/1-го посещения" type="text" name="price_first_time"></td>
+                        <td class="" style="width: 15%; border-top: none; height: 35px;"><input style="height: 35px; width: 100%" placeholder=" Цена/последующие" type="text" name="price_after"></td>
                         <td style="border-top: none;">
-                        <select class="form-control" id="diractions_select" style="min-height: 35px;" name="direction"
-                                onmousedown="if(this.options.length>5){this.size=5;}"
-                                onchange="this.blur()" onblur="this.size=0;" >
-                            <option style="color: lightgray;" >Выберите категорию</option>
+                        <select class="form-control" id="diractions_select" style="min-height: 35px;" name="direction"onmousedown="if(this.options.length>5){this.size=5;}" onchange="this.blur()" onblur="this.size=0;" >
+                            <option style="color: lightgray;" >Направление</option>
                             <?php $res = $directions->getDirections();
                             foreach ($res as $key => $value):?>
                                 <option class="nnn"
@@ -53,9 +45,9 @@
 
                 <tr>
                     <form method="POST">
-                        <td class="" style="width: 50px; border-top: none" ><input type="text" class="diract_input" name="specialty"         value="<?= $item['specialty'] ?>">       </td>
-                        <td class="" style="width: 50px; border-top: none"><input type="text" style="height: 35px;" class="" name="price_first_time"  value="<?= $item['price_first_time'] ?>"></td>
-                        <td class="" style="width: 50px; border-top: none"><input type="text" style="height: 35px;" class="" name="price_after"       value="<?= $item['price_after'] ?>">     </td>
+                        <td class="" style="width: 63%; border-top: none"><input style="height: 35px; width: 100%" type="text" class="diract_input" name="specialty"         value="<?= $item['specialty'] ?>">       </td>
+                        <td class="" style="width: 10%; border-top: none"><input style="height: 35px; width: 100%" type="text" style="height: 35px;" class="" name="price_first_time"  value="<?= $item['price_first_time'] ?>"></td>
+                        <td class="" style="width: 10%; border-top: none"><input style="height: 35px; width: 100%" type="text" style="height: 35px;" class="" name="price_after"       value="<?= $item['price_after'] ?>">     </td>
                         <td class="" style="border-top: none"">
                         <input type="button" class="btn btn-danger bt_del" data-toggle="modal" data-target="#cc<?= $item['id'] ?>" value="Удалить"    style="" >
                         <input type="submit" class="btn btn-primary bt_del" value="Сохранить" formaction="<?= $_SERVER['PHP_SELF'] ?>?page=savepriceID"  style="">
@@ -102,20 +94,14 @@
             <h3>Создание нового прайса с выездом на дом</h3>
             <form method="POST">
                 <table class="table ">
-                    <tr class="">
-                        <th>Название услуги</th>
-                        <th>Цена, грн</th>
-                        <th>Спациализация</th>
-                        <th></th>
-                    </tr>
                     <tr>
-                        <td class="" style="width: 50px; border-top: none" ><input type="text" name="specialty"></td>
-                        <td class="" style="width: 50px; border-top: none" ><input type="text" name="consulting_at_home"></td>
-                        <td style=" border-top: none; width: 255px;">
-                            <select class="form-control" id="diractions_select" name="direction" style="min-height: 35px"
+                        <td class="" style="width: 63%;  border-top: none" ><input style="height: 35px; width: 100%"   placeholder=" Название услуги" type="text" name="specialty"></td>
+                        <td class="" style="width: 10%;  border-top: none" ><input style="height: 35px; width: 100%"  placeholder=" Цена, грн" type="text" name="consulting_at_home"></td>
+                        <td          style="width: 17%;  border-top: none; ">
+                            <select class="form-control" id="diractions_select" name="direction" style="min-height: 35px; width: 100%"
                                     onmousedown="if(this.options.length>5){this.size=5;}"
                                     onchange="this.blur()" onblur="this.size=0;">
-                                <option style="color: lightgray">Выберите категорию</option>
+                                <option style="color: lightgray">Направление</option>
                                 <?php $res = $directions->getDirections();
                                 foreach ($res as $key => $value):?>
                                     <option class="nnn"
@@ -131,7 +117,7 @@
                 </table>
             </form>
             <!--  Редактирование блока с выездом на дом -->
-            <h4>Редактирование основного прайса с выездом на дом</h4>
+            <h4>Редактирование основного прайса дополнительных услуг</h4>
             <table class="table ">
                 <tr class="">
 <!--                    <th>Specialty</th>-->
@@ -144,20 +130,19 @@
 
                 <tr>
                     <form method="POST">
-                    <td class="" style="width: 250px; border-top: none" >
-                        <input  type="text" name="specialty" value="<?= $item['specialty'] ?>">
+                    <td class="" style="width: 73%; border-top: none" >
+                        <input  style="width: 100%" type="text" name="specialty" value="<?= $item['specialty'] ?>">
                     </td>
-                    <td class="" style="width: 250px; border-top: none">
-                        <input  type="text" name="consulting_at_home" value="<?= $item['consulting_at_home'] ?>">
+                    <td class="" style="width: 10%; border-top: none">
+                        <input  style="width: 100%" type="text" name="consulting_at_home" value="<?= $item['consulting_at_home'] ?>">
                     </td>
                     <td style="border-top: none">
 
-                        <input type="hidden" value="<?= $item['id'] ?>" name="priceID">
+                        <input style="width: 100%" type="hidden" value="<?= $item['id'] ?>" name="priceID">
 
                        <input type="button" class="btn btn-danger bt_del" data-toggle="modal" data-target="#c<?= $item['id'] ?>" value="Удалить"    style="" >
 
-                        <input type="submit" class="btn btn-primary bt_del" value="Сохранить"
-                               formaction="<?= $_SERVER['PHP_SELF'] ?>?page=savepriceHID"  style="">
+                        <input type="submit" class="btn btn-primary bt_del" value="Сохранить" formaction="<?= $_SERVER['PHP_SELF'] ?>?page=savepriceHID"  style="">
 
 
                     </td>
