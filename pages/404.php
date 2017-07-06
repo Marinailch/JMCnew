74c5e589f8b3be72a82b9dfda9f0fb7151b297f9
Marinailch <marina_ilch@mail.ru>
2017-07-06 13:02:48 +0300

    404.php

---
+++ pages/404.php
@@ -0,0 +1,16 @@
+
+
+
+
+<div class="conteiner">
+    <div class="row">
+        <div class="col-sm-12">
+        <div style="display: block; overflow: hidden; margin-top: 200px; margin: 0 auto;">
+             <h1>Ошибка 404</h1>
+            <h2>Извенте, данной страницы  не существует </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
