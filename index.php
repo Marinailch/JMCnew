661a5a472190fad794ccd0a0ee1cb164789f0390
s7eel <s7eell@gmail.com>
2017-07-13 23:42:47 +0300

    issues with index.php

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php
-error_reporting(E_ALL);
+ini_set('display_errors','0');
 //Настройки приложения
 include_once 'Config/config.php';
 //Возврат классов, подключаемых к приложению
 include_once 'Config/ConfigClasses.php';
 
-
+$page = $request->getMain();
 
 include_once "header.php";
 
-
-$page = $request->getMain();
-include_once $page;
-
+include_once $page[0];
 
 include_once "footer.php";
-
